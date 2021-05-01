d4572b7d5f3f32233c05c2226f213b30a26aa6c2
A-GHOSH-dev <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-05-01 13:37:15 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -1,26 +1,8 @@
-const year = new Date().getFullYear();
-const choosenDate = new Date(year, 7, 6).getTime();
+const items = document.querySelectorAll(".accordion a");
  
-let countdown = setInterval(function() {
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
  
-  const today = new Date().getTime();
- 
-  const diff = choosenDate - today;
- 
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
- 
-  document.getElementById("countdown").innerHTML =
-    "<div class=\"days\"> \
-  <div class=\"c-number\">" + days + "</div>days</div> \
-<div class=\"hours\"> \
-  <div class=\"c-number\">" + hours + "</div>hours</div> \
-<div class=\"minutes\"> \
-  <div class=\"c-number\">" + minutes + "</div>minutes</div> \
-<div class=\"seconds\"> \
-  <div class=\"c-number\">" + seconds + "</div>seconds</div> \
-  ";
- 
-}, 1000);
+items.forEach(item => item.addEventListener('click', toggleAccordion));
